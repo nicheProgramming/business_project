f239f26f377c5d63ab902e492087da11ae3b1e1d
Christopher <captainless2@gmail.com>
2016-04-06 17:01:04 +0000

    Basic header added

---
+++ header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <?php
+        
+    ?>
+</head>
+
+<body id="bar">
+    <div id="name">PRSoftworks</div>
+    <div id="headerText">
+        <a href="">Home</a> / 
+        <a href="">Our Work</a> /
+        <a href="">Contact us</a>
+    </div>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>PRSoftworks</title>
+    <?php require('header.php');?>
+</head>
+
+<body>
+
+</body>
+</html>
