af76a353bf17dfd69b57f99c58261b9e3b122001
Christopher <captainless2@gmail.com>
2016-04-12 13:52:59 +0000

    Set up page, ready for module integration

--- header.php
+++ header.php
@@ -1,18 +1,15 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <link rel="stylesheet" type="text/css" href="style.css">
-    <?php
-        
-    ?>
-</head>
+<link rel="stylesheet" type="text/css" href="style.css">
+<link rel='icon' href='blob:https%3A//drive.google.com/5201cb7f-f571-4739-adff-92e1af43136c' type='image/x-icon' >
 
-<body id="bar">
-    <div id="name">PRSoftworks</div>
-    <div id="headerText">
-        <a href="">Home</a> / 
-        <a href="">Our Work</a> /
-        <a href="">Contact us</a>
+<header>
+    <div id="bar">
+        <div id="name">
+            PRSoftworks
+        </div>
+        <div id="headerText">
+            <a id="home" href="index">Home</a> / 
+            <a id="work" href="work">Our Work</a> /
+            <a id="contact" href="contact">Contact us</a>
+        </div>
     </div>
-</body>
-</html>
+</header>
--- index.php
+++ index.php
@@ -1,11 +1,48 @@
 <!DOCTYPE html>
 <html>
+    
 <head>
     <title>PRSoftworks</title>
-    <?php require('header.php');?>
+    <?php require('header.php'); ?>
+    <script>
+        document.getElementById("home").style.color = '#000000';
+    </script>
 </head>
 
 <body>
-
+    <div id="page">
+        <div id='intro'>
+            PRSoftworks - Software for the common man
+        </div>
+        <p>
+            The purpose of PRSoftworks is to help individuals 
+            and companies get the software they need. The 
+            company was founded by four individuals, who are 
+            to date their only employees. Short term, 
+            PRSoftworks wants to help establishments in the 
+            local area attain the software they need to do 
+            their jobs, with a focus on web apps. Long term, 
+            as the company grows, we want to help as many 
+            people worldwide accomplice great things with the 
+            software we create. Computer languages we use 
+            include, but are not limited to, HTML, CSS, 
+            Javascript, and PHP. We require 4 business grade 
+            workstations with the appropriate software 
+            accomodations to do our jobs efficiently. To cover 
+            initial costs, profits in the first month must be 
+            upwards of $4000. However, this can likely be 
+            spread out over a year or so to make necessary 
+            income / month much smaller. Our advertisement 
+            will be largely word-of-mouth, and we will 
+            frequently email potential clients with ideas to 
+            help drive their businesses forward, while 
+            including recommendations from previous customers.  
+        </p>
+    </div>
 </body>
+
+<footer>
+    <?php require('footer.php'); ?>
+</footer>
+
 </html>
